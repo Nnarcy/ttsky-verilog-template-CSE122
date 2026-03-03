869dd26b5db8667fcfa2dc7e950876603b3935fd
Nicholas Narcy <nnarcy3652@gmail.com>
2026-03-02 22:17:20 -0800

    updated verilog for simple ALU

--- src/project.v
+++ src/project.v
@@ -17,11 +17,38 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  wire [2:0] a  = ui_in[2:0];
+  wire [2:0] b  = ui_in[5:3];
+  wire [1:0] op = ui_in[7:6];
+
+  wire [7:0] add_output = {5'b00000, a} + {5'b00000, b};
+  wire [7:0] sub_output = {5'b00000, a} - {5'b00000, b};
+  wire [7:0] mul_output = a * b;
+  wire [7:0] xor_output = {5'b00000, (a ^ b)};
+
+  reg [7:0] y;
+  always @(*) begin 
+    case (op)
+      2'b00: y = add_output;
+      2'b01: y = sub_output;
+      2'b10: y = mul_output;
+      2'b11: y = xor_output;
+      default: y = 8'h00;
+    endcase
+  end
+
+  assign uo_out = y;
+
+  assign uio_out = 8'h00;
+  assign uio_oe  = 8'h00;
+
+  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
