84ac4d2c77b0bb8170b24170e356feca581143f9
Nicholas Narcy <nnarcy3652@gmail.com>
2026-03-03 01:59:50 -0800

    new push for tests

--- src/project.v
+++ src/project.v
@@ -23,7 +23,6 @@ module tt_um_nnarcy_addsubmultxor (
 
   // List all unused inputs to prevent warnings
   // wire _unused = &{ena, clk, rst_n, 1'b0};
-  //
 
   wire [2:0] a  = ui_in[2:0];
   wire [2:0] b  = ui_in[5:3];
